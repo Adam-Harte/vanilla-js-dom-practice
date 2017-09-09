238b5c0f2f301606ddc0004c8e6ddee6db10d177
Adam-Harte <adamharte11@hotmail.co.uk>
2017-09-09 22:29:35 +0100

    add book feature added to js file

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var list = document.querySelector('#book-list ul');
 var forms = document.forms;
-var liElem = document.createElement('li');
-var bookName = document.createElement('span');
-var deleteBtn = document.createElement('span');
+
+var addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    var value = addForm.querySelector('input[type="text"').value;
+    //create elements
+    var liElem = document.createElement('li');
+    var bookName = document.createElement('span');
+    var deleteBtn = document.createElement('span');
+    //add text content
+    bookName.textContent = value;
+    deleteBtn.textContent = 'delete';
+    //add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+    //append elements
+    liElem.appendChild(bookName);
+    liElem.appendChild(deleteBtn);
+    list.appendChild(liElem)
+});
