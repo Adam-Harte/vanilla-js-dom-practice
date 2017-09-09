a15231c9acdcce8add2e3a6d2ed53a16db21fc69
Adam-Harte <adamharte11@hotmail.co.uk>
2017-09-09 22:00:28 +0100

    added dom elements in js file

---
+++ index.js
@@ -0,0 +1,5 @@
+var list = document.querySelector('#book-list ul');
+var forms = document.forms;
+var liElem = document.createElement('li');
+var bookName = document.createElement('span');
+var deleteBtn = document.createElement('span');
